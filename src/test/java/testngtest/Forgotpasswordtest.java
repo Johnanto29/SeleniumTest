2b924fad7deac5749367458a9df24e46a10f9867
HP <HP@DESKTOP-O3TKCG4.home>
2024-12-18 21:11:07 -0500

    My edited version

--- src/test/java/testngtest/Forgotpasswordtest.java
+++ src/test/java/testngtest/Forgotpasswordtest.java
@@ -25,26 +25,35 @@ public class Forgotpasswordtest {
 
 	@Test
 	public void forgotpassword() {
-        // Select Forgot Password
+		// Select Forgot Password
 		WebElement ele = driver.findElement(By.linkText("Forgot Password"));
 		ele.click();
-        // page title check
+		// Page Title check
 		String pagetitle = driver.getTitle();
 		Assert.assertEquals(pagetitle, "The Internet");
-		
+		// Page Footer check
+		WebElement Footermsg = driver.findElement(By.xpath("//*[@id='page-footer']/div"));
+		String msg = Footermsg.getText();
+		Assert.assertEquals(msg, "Powered by Elemental Selenium");
+
 	}
-	
+
+	@Test
 	public void forgotpasswordexe() {
-		//Enter email ID
-				WebElement enteremail = driver.findElement(By.xpath("//*[@id='email']"));
-				enteremail.sendKeys("hi.john.mba@gmail.com");
-				//Click Retrieve button
-				WebElement Retrievebutton = driver.findElement(By.xpath("//*[@id='form_submit']"));
-				Retrievebutton.click();
-				// message confirmation
-				WebElement confirmationmsg = driver.findElement(By.xpath("/html/body/h1"));
-				Assert.assertEquals(confirmationmsg, "Internal Server Error");
-		
+		// Select Forgot Password
+		WebElement ele = driver.findElement(By.linkText("Forgot Password"));
+		ele.click();
+		// Enter email ID
+		WebElement enteremail = driver.findElement(By.xpath("//*[@id='email']"));
+		enteremail.sendKeys("hi.john.mba@gmail.com");
+		// Click Retrieve button
+		WebElement Retrievebutton = driver.findElement(By.xpath("//*[@id='form_submit']"));
+		Retrievebutton.click();
+		// message confirmation
+		WebElement confirmationmsg = driver.findElement(By.xpath("/html/body/h1"));
+		String msg2 = confirmationmsg.getText();
+		Assert.assertEquals(msg2, "Internal Server Error");
+
 	}
 
 }
